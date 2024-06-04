d1c3a52ed4631cec0faa92d6d7cde5af905ac910
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-04 17:34:10 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,7 @@ import movies from "./data/movies";
 function App() {
   return (
     <div className="App">
-      <section className="movie-list-section">
-        {movies.map((item, index) => (
-          <div key={index}>
-            <li>
-              <img src={item.image} className="imgMovie" />
-            </li>
-            <li>title: {item.title}</li>
-            <li>year: {item.year}</li>
-            <li>runtime: {item.runtime}</li>
-            <li>director: {item.director}</li>
-            <li>genres: {item.genres}</li>
-            <li>imdbRating: {item.imdbRating}</li>
-            <li>imdbVotes: {item.imdbVotes}</li>
-          </div>
-        ))}
-      </section>
+      <h1>hello view</h1>
     </div>
   );
 }
